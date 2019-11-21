5b7db3e99ef1ab2b000bee302f712a6b507c4bc5
Erwin Romkes <eromkes@dunique.nl>
2019-11-21 13:15:51 +0100

    Init module structure
    With a little help from mage2gen.com

---
+++ Helper/Data.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * With this module you can set-up payment methods for each customer
+ * Copyright (C) 2019 
+ * 
+ * This file included in StoneandStorm/CustomerPaymentMethod is licensed under OSL 3.0
+ * 
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace StoneandStorm\CustomerPaymentMethod\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+
+class Data extends AbstractHelper
+{
+
+    /**
+     * @param \Magento\Framework\App\Helper\Context $context
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context
+    ) {
+        parent::__construct($context);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return true;
+    }
+}
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'StoneandStorm_CustomerPaymentMethod',
+    __DIR__
+);
